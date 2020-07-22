408c8ccef12afea2a4788b7dd975279b79f54454
Timothy <exzos_graalonline@hotmail.com>
2020-07-21 20:12:00 -0400

    Redid Sidebar component with hooks

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Drawer } from '@material-ui/core';
 
@@ -9,31 +9,25 @@ import ClearAllIcon from '@material-ui/icons/ClearAll';
 import GeneralComponent from './GeneralComponent';
 import '../styles/sidebar.scss';
 
-// TODO: new message indicators
-class Sidebar extends GeneralComponent {
-  componentDidMount(props) {
-    this.sidebarContainer = document.getElementsByClassName('MuiDrawer-paper')[0];
+function Sidebar(props) {
+  const generalComponent = new GeneralComponent();
 
-    this.pushMainContent();
-  }
+  const sidebarContainer = useRef(null);
+  const users = generalComponent.getSessionItem('users');
 
-  componentDidUpdate() {
-    this.pushMainContent();
-  }
+  useEffect(() => {
+    sidebarContainer.current = document.getElementsByClassName('MuiDrawer-paper')[0];
 
-  pushMainContent = () => {
-    if(this.sidebarContainer) {
-      document.body.style.marginLeft = this.sidebarContainer.offsetWidth + 'px';
+    if(sidebarContainer.current) {
+      document.body.style.marginLeft = sidebarContainer.current.offsetWidth + 'px';
     }
-  }
+  });
 
-  renderSpaces = () => {
-    const users = this.getSessionItem('users');
-
+  const renderSpaces = () => {
     if(users) {
       return Object.values(users).map(user => (
         <div key={user.space} className="spaceNavItem ">
-          <Link className={(user.space === this.props.spaceID) ? "active" : ""} tabIndex="-1" to={{
+          <Link className={(user.space === props.spaceID) ? "active" : ""} tabIndex="-1" to={{
             pathname: "/r/",
             state: {
               pathname: "/s/",
@@ -53,34 +47,31 @@ class Sidebar extends GeneralComponent {
     }
   }
 
-  // TEMP clear link
-  render() {
-    return (
-      <Drawer id="sidebarContainer" variant="permanent">
-        <div id="sidebar">
-          <div className="logo">
-            <LandscapeIcon fontSize="large" />
-          </div>
-
-          <div className="spaceNav">
-            {this.renderSpaces()}
-          </div>
-
-          <div className="menuItem">
-            <Link to="/" tabIndex="-1">
-              <AddCircleOutlineIcon fontSize="large" />
-            </Link>
-          </div>
-
-          <div className="menuItem clear">
-            <Link to="/" tabIndex="-1" onClick={this.clearSession}>
-              <ClearAllIcon fontSize="large" />
-            </Link>
-          </div>
+  return (
+    <Drawer id="sidebarContainer" variant="permanent">
+      <div id="sidebar">
+        <div className="logo">
+          <LandscapeIcon fontSize="large" />
         </div>
-      </Drawer>
-    );
-  }
+
+        <div className="spaceNav">
+          {renderSpaces()}
+        </div>
+
+        <div className="menuItem">
+          <Link to="/" tabIndex="-1">
+            <AddCircleOutlineIcon fontSize="large" />
+          </Link>
+        </div>
+
+        <div className="menuItem clear">
+          <Link to="/" tabIndex="-1" onClick={generalComponent.clearSession}>
+            <ClearAllIcon fontSize="large" />
+          </Link>
+        </div>
+      </div>
+    </Drawer>
+  );
 }
 
 export default Sidebar;
